ef67edd5c458ce209ffd8746192af5d1b0b787d0
Rafa <serra.raf@gmail.com>
2025-10-02 16:32:53 +0200

    igualar con github

--- dist/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRoutes = void 0;
-// src/routes/auth.routes.ts
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth.controller");
-const router = (0, express_1.Router)();
-exports.authRoutes = router;
-router.post('/', auth_controller_1.loginUser);
--- dist/utils/helper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseDate = void 0;
-const parseDate = (value) => {
-    return value ? new Date(value) : undefined;
-};
-exports.parseDate = parseDate;
